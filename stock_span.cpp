bd7d878e5bc1743590a1928980b4f4669311ac37
Harshit Ranjan <ara.harshitraj@gmail.com>
2026-06-12 13:01:57 +0530

    Add files via upload

---
+++ stock_span.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+(vector<int>& price) 
+    vector<int> span(price.size());
+
+    stack<int> st;
+
+
+    for (int i = 0; i < price.size(); i++) {
+
+        st.push(i);
+    }
+    return span;
+
+
+
+int main() {
+    vector<int> price = {100, 80, 60, 70, 60, 75, 85};
+
+    for(int i = 0; i < ans.size(); i++) {
+    cout << ans[i] << " ";
+}
+
+    return 0;
+}
